fee37a72698e6ddf810df03c2d13b4779a6e8999
Eliminater74 <eliminater74@gmail.com>
2024-12-15 16:11:44 -0500

    Add project files.

---
+++ MetaQuestTrayManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MetaQuestTrayManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
