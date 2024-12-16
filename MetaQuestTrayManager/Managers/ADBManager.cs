c95bbee512007ff82c430294dd819d46a153d532
Eliminater74 <eliminater74@gmail.com>
2024-12-15 19:43:26 -0500

    Update ADB manager

--- MetaQuestTrayManager/Managers/ADBManager.cs
+++ MetaQuestTrayManager/Managers/ADBManager.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace MetaQuestTrayManager.Managers
 {
-    internal class ADBManager
+    public class ADBManager
     {
+        private readonly string adbPath;
+
+        public ADBManager()
+        {
+            adbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Binaries", "adb.exe");
+        }
+
+        public string RunCommand(string arguments)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = adbPath,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+
+        public List<string> GetConnectedDevices()
+        {
+            string output = RunCommand("devices");
+            var devices = new List<string>();
+
+            foreach (var line in output.Split('\n'))
+            {
+                if (line.Contains("\tdevice"))
+                {
+                    devices.Add(line.Split('\t')[0]);
+                }
+            }
+            return devices;
+        }
     }
 }
